b34f3f85e4196f955664f078bc1dab65960c5b34
Airis <104155192+OnlyMeST@users.noreply.github.com>
2024-01-17 11:17:02 +0100

    Update VigenereDecryption.java

--- VigenereDecryption.java
+++ VigenereDecryption.java
@@ -1,3 +1,36 @@
+public static int findLikelyKey(String substring) {
+        int[] frequencies = new int[26];
+        for (int i = 0; i < substring.length(); i++) {
+            char c = Character.toUpperCase(substring.charAt(i));
+            if (Character.isLetter(c)) {
+                frequencies[c - 'A']++;
+            }
+        }
+
+        int maxFrequency = 0;
+        int likelyKey = 0;
+        for (int i = 0; i < frequencies.length; i++) {
+            if (frequencies[i] > maxFrequency) {
+                maxFrequency = frequencies[i];
+                likelyKey = (i - ('E' - 'A') + 26) % 26;
+            }
+        }
+
+        return likelyKey;
+    }
+
+    public static String decryptVigenere(String ciphertext, int key) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char encryptedChar = ciphertext.charAt(i);
+            char keyChar = (char) ((encryptedChar - key + 26) % 26 + 'A');
+            decryptedText.append(keyChar);
+        }
+        return decryptedText.toString();
+    }
+
+
+
 public static String decryptVigenere(String ciphertext, int key) {
         StringBuilder decryptedText = new StringBuilder();
         for (int i = 0; i < ciphertext.length(); i++) {
